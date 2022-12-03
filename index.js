ed3e3f1794d21aeaeaa19d6bf13f249812400c2c
Loise <loise.kimani@student.moringaschool.com>
2022-12-04 01:45:35 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// Write your code 
+
+const nope = document.querySelector("#main")
+nope.remove()
+
+const newHeader = document.createElement('h1')
+
+newHeader.setAttribute('id', 'victory')
+
+
+ newHeader.textContent = 'Loise Kimani is the champion'
